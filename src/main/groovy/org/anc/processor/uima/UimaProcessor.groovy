57fc54e096a5a775d598da6f3bbebe28ef8ed355
Keith Suderman <suderman@anc.org>
2015-06-10 16:25:13 -0400

    Fixed merge conflict.

--- src/main/groovy/org/anc/processor/uima/UimaProcessor.groovy
+++ src/main/groovy/org/anc/processor/uima/UimaProcessor.groovy
@@ -6,11 +6,20 @@ import javax.ws.rs.Path
 @Path("/uima")
 class UimaProcessor extends AbstractProcessor{
 
+//<<<<<<< Updated upstream
     public UimaProcessor() {
         super(["f.penn", "f.sentences", "f.biber", "f.c5", "f.c7", "f.cb",
                "f.content", "f.event", "f.hepple", "f.logical",
                "f.mpqa", "f.nc", "f.ne", "f.none","f.slate_coref",
                "f.vc", "f.ptb", "f.ptbtok", "f.fn", "f.fntok", "f.s"] as List<String>)
+//=======
+//    public static final Set<String> ACCEPTABLE =
+//            ["f.penn", "f.s", "f.cb", "f.event", "f.fn", "f.fntok",
+//             "f.logical", "f.mpqa", "f.ns", "f.ne", "f.ptb", "f.ptbtok",
+//             "f.vc"] as HashSet<String>
+//
+//    public UimaProcessor() {
+//>>>>>>> Stashed changes
         processor = new org.anc.tool.uima.UimaProcessor()
     }
 }
